ccdf2e1417bc3ca57c0c1e2c896ba3fe31914e7b
Jesus Morales <morales.jesusant@gmail.com>
2026-01-14 22:09:06 -0800

    Updated LAB1A in order to show my program's usability

--- Module1/Lab1a/funcTemplates.cpp
+++ Module1/Lab1a/funcTemplates.cpp
@@ -11,9 +11,30 @@ T maxElement(T arg1, T arg2);
 
 int main() {
 
-    cout << "Min: " << minElement("helloo", "world") << endl;
-    cout << "Max: " << maxElement("helloo", "world") << endl;
+    string str1 = "hello", 
+           str2 = "world";
 
+    int num1 = 10,
+        num2 = 15;
+
+    double dec1 = 15.3,
+           dec2 = 18.67;
+
+    char char1 = 'g',
+         char2 = 'Y';
+
+    cout << "\nMin of " << "( " << str1 << ", " << str2 << " )" << " => " << minElement(str1, str2) << endl;
+    cout << "Max of " << "( " << str1 << ", " << str2 << " )" << " => " << maxElement(str1, str2) << endl;
+
+    cout << "\nMin of " << "( " << num1 << ", " << num2 << " )" << " => " << minElement(num1, num2) << endl;
+    cout << "Max of " << "( " << num1 << ", " << num2 << " )" << " => " << maxElement(num1, num2) << endl;
+    
+    cout << "\nMin of " << "( " << dec1 << ", " << dec2 << " )" << " => " << minElement(dec1, dec2) << endl;
+    cout << "Max of " << "( " << dec1 << ", " << dec2 << " )" << " => " << maxElement(dec1, dec2) << endl;
+    
+    cout << "\nMin of " << "( " << char1 << ", " << char2 << " )" << " => " << minElement(char1, char2) << endl;
+    cout << "Max of " << "( " << char1 << ", " << char2 << " )" << " => " << maxElement(char1, char2) << endl;
+    
     return 0;
 }
 
