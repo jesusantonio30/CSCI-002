30117734b2e6655060d2ca8fd2d48d9fda193653
Jesus Morales <morales.jesusant@gmail.com>
2026-01-26 16:45:28 -0800

    Implemented the logic for assignment Lab1E Employee Class. Not done yet, though

---
+++ Module1/Lab1e/employeeClass.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+    private:
+        string firstName, lastName;
+        int age;
+
+    public:
+        Person(string argFN = "John", string argLN = "Doe", int argAge = 20);
+
+        void setFName(string argFname);
+        void setLName(string argLname);
+        void setAge(int argAge);
+
+        string getFName();
+        string getLName();
+        int getAge();
+
+        virtual void display();
+};
+
+class UScitizen : public Person {
+    private:
+        int SSN;
+
+    public:
+        UScitizen(string argFN = "Jane", string argLN = "Doe", int argAge = 21, int argSSN = 123456789);
+
+        void setSSN(int argSSN);
+        int getSSN();
+
+        virtual void display();
+};
+
+class Employee : public UScitizen {
+    private:
+        int employeeID;
+        float salary;
+
+    public:
+        Employee(string argFN = "Jane", string argLN = "Doe", int argAge = 21, int argSSN = 123456789, int argEmployeeID = 99999, float argComp = 100000.00);
+
+        void setEmployeeID(int argID);
+        int getEmployeeID();
+
+        void setSalary(float argComp);
+        float getSalary();
+
+        virtual void display();
+};
+
+int main() {
+
+    UScitizen citizen1("Jesus", "Morales", 23, 614738526);
+    UScitizen citizen2("Maria", "Gamez", 21, 614768226);
+    UScitizen citizen3("Fatima", "Morales", 20, 617338526);
+
+    Person person1("Antonio", "Morales", 65);
+    Person person2("Herminia", "Bermudez", 62);
+
+    citizen1.display();
+    cout << '\n';
+    citizen2.display();
+    cout << '\n';
+    citizen3.display();
+    cout << '\n';
+    person1.display();
+    cout << '\n';
+    person2.display();
+
+
+    return 0;
+}
+
+// PERSON ----------------------------------------------------------------------------------------
+Person::Person(string argFN, string argLN, int argAge) {
+    firstName = argFN;
+    lastName = argLN;
+    age = argAge;
+}
+
+void Person::setFName(string argFname) {
+    firstName = argFname;
+}
+
+void Person::setLName(string argLname) {
+    lastName = argLname;
+}
+
+void Person::setAge(int argAge) {
+    age = argAge;
+}
+
+
+string Person::getFName() {
+    return firstName;
+}
+
+string Person::getLName() {
+    return lastName;
+}
+
+int Person::getAge() {
+    return age;
+}
+
+
+void Person::display() {
+    cout << "First Name: " << getFName() << endl;
+    cout << "Last Name: " << getLName() << endl;
+    cout << "Age: " << getAge() << endl;
+}
+
+// USCITIZEN ----------------------------------------------------------------------------------------
+UScitizen::UScitizen(string argFN, string argLN, int argAge, int argSSN):Person(argFN, argLN, argAge) {
+    setSSN(argSSN);
+}
+
+void UScitizen::setSSN(int argSSN) {
+    SSN = argSSN;
+}
+
+int UScitizen::getSSN() {
+    return SSN;
+}
+
+void UScitizen::display() {
+    Person::display();
+    cout << "SSN: " << getSSN() << endl;
+}
+
+// EMPLOYEE ----------------------------------------------------------------------------------------
+Employee::Employee(string argFN, string argLN, int argAge, int argSSN, int argEmployeeID, float argComp):UScitizen(argFN, argLN, argAge, argSSN) {
+    setEmployeeID(argEmployeeID);
+    setSalary(argComp);
+}
+
+void Employee::setEmployeeID(int argID) {
+    employeeID = argID;
+}
+
+int Employee::getEmployeeID() {
+    return employeeID;
+}
+
+void Employee::setSalary(float argComp) {
+    salary = argComp;
+}
+
+float Employee::getSalary() {
+    return salary;
+}
+
+void Employee::display() {
+    UScitizen::display();
+    cout << "Employee ID: " << getEmployeeID() << endl;
+    cout << "Salary: " << getSalary() << endl;
+}
+
+
