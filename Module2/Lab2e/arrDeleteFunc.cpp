698c56b8312ef09c56ccba70604c336be5e016a7
Jesus Morales <morales.jesusant@gmail.com>
2026-02-01 14:55:29 -0800

    WIP: Lab2E and Lab2F

--- Module2/Lab2e/arrDeleteFunc.cpp
+++ Module2/Lab2e/arrDeleteFunc.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void deleteElement(T* arr, int index, int size);
+
+template <class T>
+void printArray(const T* a, int size);
 
 int main() {
 
+    const int size = 9;
+
+    int array[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    deleteElement<int>(array, 2, size);
+
+    printArray(array, size);
+
     return 0;
+}
+
+template <class T>
+void deleteElement(T* arr, int index, int size) {
+
+    for (int i = index; i < size; ++i) {
+        arr[i] = arr[i+1];
+    }
+}
+
+template <class T>
+void printArray(const T* a, int size) {
+    cout << "[ ";
+    for (int i = 0; i < size; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "]\n";
 }
--- Module2/Lab2f/arrMergeFunc.cpp
+++ Module2/Lab2f/arrMergeFunc.cpp
@@ -4,3 +4,12 @@ int main() {
 
     return 0;
 }
+
+template <class T>
+T* arrMerger(T* arr1, int size1, T* arr2, int size2) {
+    T* mergedArr[size1 + size2] = arr1;
+
+    for (int i = 0; i < size2; ++i) {
+        mergedArr[size1] = 
+    }
+}
