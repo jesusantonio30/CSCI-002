3bafa12bc4704b2ea535ede2687dff75642432c2
Jesus Morales <morales.jesusant@gmail.com>
2026-02-02 20:51:02 -0800

    Completed Lab2e Array Delete Function

--- Module2/Lab2e/arrDeleteFunc.cpp
+++ Module2/Lab2e/arrDeleteFunc.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -14,10 +13,20 @@ int main() {
     const int size = 9;
 
     int array[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
     deleteElement<int>(array, 2, size);
-
     printArray(array, size);
+
+    int array2[size] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    deleteElement<int>(array2, 0, size);
+    printArray(array2, size);
+
+    double array4[size] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9};
+    deleteElement<double>(array4, 3, size);
+    printArray(array4, size);
+
+    char array5[size] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+    deleteElement<char>(array5, 0, size);
+    printArray(array5, size);
 
     return 0;
 }
@@ -28,6 +37,8 @@ void deleteElement(T* arr, int index, int size) {
     for (int i = index; i < size; ++i) {
         arr[i] = arr[i+1];
     }
+
+    arr[size-1] = 0;
 }
 
 template <class T>
